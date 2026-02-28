aa312ac584a0d690564856d062d519a1e22dccfb
Fahad Hassan <fahad.1979.hassan@gmail.com>
2026-02-28 05:01:42 +0500

    Add ProductController.java

---
+++ src/main/java/ledge/application/ProductController.java
@@ -0,0 +1,37 @@
+package ledge.application;
+
+import ledge.domain.Product;
+import ledge.domain.ProductService;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class ProductController {
+    private final ProductService productService;
+    private final InventoryEventBroker eventBroker;
+
+    public ProductController(ProductService productService, InventoryEventBroker eventBroker) {
+        this.productService = productService;
+        this.eventBroker = eventBroker;
+        
+        this.eventBroker.subscribe(ProductAddedEvent.class, this::handleProductAdded);
+        this.eventBroker.subscribe(ProductRemovedEvent.class, this::handleProductRemoved);
+    }
+
+    private void handleProductAdded(ProductAddedEvent event) {
+        productService.addProduct(event.getProduct());
+    }
+
+    private void handleProductRemoved(ProductRemovedEvent event) {
+        productService.deleteProduct(event.getProductId());
+    }
+
+    public Optional<Product> getProductById(UUID id) {
+        return productService.getProductById(id);
+    }
+
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
