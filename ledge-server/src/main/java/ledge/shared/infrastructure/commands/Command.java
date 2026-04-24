30abf92ea7c0a3a17288d7f247ec70ed84519c52
Fahad Hassan <fahad.1979.hassan@gmail.com>
2026-04-25 03:28:50 +0500

    refactor: update Command and Query interfaces to use PermissionDTO instead of internal Permission model

--- ledge-server/src/main/java/ledge/shared/infrastructure/commands/Command.java
+++ ledge-server/src/main/java/ledge/shared/infrastructure/commands/Command.java
@@ -2,13 +2,13 @@ package ledge.shared.infrastructure.commands;
 
 import java.util.Optional;
 
-import ledge.security.internal.domain.models.Permission;
+import ledge.security.api.dto.PermissionDTO;
 
 /**
  * Represents an intent to change the state of the system.
  */
 public interface Command {
-    default Optional<Permission> getRequiredPermission() {
+    default Optional<PermissionDTO> getRequiredPermission() {
         return Optional.empty();
     }
 }
